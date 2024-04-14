74416b54406b5a1be95edfa902c76ef8f80e5f18
lucasgpulcinelli <lucasegp@usp.br>
2024-04-14 18:49:56 -0300

    Add box to choose algorithm and show image

--- src/ui.py
+++ src/ui.py
@@ -1,5 +1,8 @@
-from PyQt5.QtGui import QStandardItem, QStandardItemModel
-from PyQt5.QtWidgets import QHBoxLayout, QPushButton, QWidget, QVBoxLayout, QLineEdit, QListView
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QStandardItem, QStandardItemModel
+from PyQt5.QtWidgets import QComboBox, QHBoxLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QSizePolicy, QStackedLayout, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QLineEdit, QListView
 
 import maze_solver
 
@@ -52,38 +55,89 @@ class RoomSearch(QWidget):
         return t
 
 
-class App(QWidget):
-    def __init__(self, tensor, rooms):
+class FromToSearch(QWidget):
+    def __init__(self, rooms):
         super().__init__()
         self.rooms = rooms
-        self.tensor = tensor
 
-        layout = QVBoxLayout()
-        sub_layout = QHBoxLayout()
+        layout = QHBoxLayout()
 
         self.room_from = RoomSearch(rooms.keys(), "onde você está")
         self.room_to = RoomSearch(rooms.keys(), "para onde você vai")
 
-        sub_layout.addWidget(self.room_from)
-        sub_layout.addWidget(self.room_to)
-
-        layout.addLayout(sub_layout)
-
-        self.button = QPushButton("Encontrar Caminho")
-        self.button.clicked.connect(self.genPath)
-
-        layout.addWidget(self.button)
+        layout.addWidget(self.room_from)
+        layout.addWidget(self.room_to)
 
         self.setLayout(layout)
 
+
+class App(QWidget):
+    def __init__(self, tensor, rooms):
+        super().__init__()
+
+        self.tensor = tensor
+        self.rooms = rooms
+
+        layout = QVBoxLayout()
+        self.stacked_layout = QStackedLayout()
+        execute_layout = QHBoxLayout()
+
+        self.search = FromToSearch(rooms)
+
+        self.button = QPushButton("Gerar Caminho")
+        self.button.clicked.connect(self.buttonToggle)
+        self.button.setSizePolicy(
+            QSizePolicy.Expanding, QSizePolicy.Ignored)
+
+        self.algorithms = QComboBox()
+        self.algorithms.addItem("Resolver usando BFS")
+        self.algorithms.addItem("Resolver Usando A*")
+
+        self.image_label = QLabel()
+
+        self.stacked_layout.addWidget(self.search)
+        self.stacked_layout.addWidget(self.image_label)
+        execute_layout.addWidget(self.button)
+        execute_layout.addWidget(self.algorithms)
+
+        layout.addLayout(self.stacked_layout)
+        layout.addLayout(execute_layout)
+
+        self.setLayout(layout)
+
+    def buttonToggle(self):
+        if self.stacked_layout.currentIndex() != 0:
+            self.stacked_layout.setCurrentIndex(0)
+            self.button.setText("Gerar Caminho")
+            return
+
+        path = self.genPath()
+        if path is None:
+            return
+
+        img = QPixmap('res/map/0.png')
+
+        self.image_label.setPixmap(img.scaledToHeight(750))
+        self.image_label.setAlignment(Qt.AlignCenter)
+        self.image_label.setScaledContents(False)
+        self.image_label.setWordWrap
+
+        self.stacked_layout.setCurrentIndex(1)
+        self.button.setText("Fazer um Novo Trajeto")
+
     def genPath(self):
-        text_from = self.room_from.selectFirst()
-        text_to = self.room_to.selectFirst()
+        text_from = self.search.room_from.selectFirst()
+        text_to = self.search.room_to.selectFirst()
 
         if text_from is None or text_to is None:
-            return
+            return None
 
         source = self.rooms[text_from]
         dest = self.rooms[text_to]
 
-        print(maze_solver.solveBFS(self.tensor, source, dest))
+        if self.algorithms.currentIndex() == 0:
+            solver = maze_solver.solveBFS
+        else:
+            solver = maze_solver.solveAStar
+
+        return solver(self.tensor, source, dest)
