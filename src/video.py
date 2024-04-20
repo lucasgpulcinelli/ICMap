65ade07d8a7246ee26518a6766cc197d15cd1799
lucasgpulcinelli <lucasegp@usp.br>
2024-04-20 16:00:17 -0300

    Show full path after solution in video

--- src/video.py
+++ src/video.py
@@ -3,15 +3,21 @@ from PIL import Image
 import cv2
 
 
-def generate_video(solution, scale=10):
+def getImgs():
     imgs = []
     for i in range(5):
         imgs.append(Image.open(f"res/map/{i}.png"))
 
-    videodims = imgs[0].size[0]*scale, imgs[0].size[1]*scale
-
     for i in range(5):
         imgs[i] = np.array(imgs[i])
+
+    return imgs
+
+
+def generate_video(solution, scale=10):
+    imgs = getImgs()
+
+    videodims = imgs[0].shape[1]*scale, imgs[0].shape[0]*scale
 
     fourcc = cv2.VideoWriter_fourcc(*'MPEG')
     video = cv2.VideoWriter("res/path.avi", fourcc, 20, videodims)
@@ -41,5 +47,17 @@ def generate_video(solution, scale=10):
                              interpolation=cv2.INTER_NEAREST_EXACT)
 
         video.write(resized)
+
+    imgs = getImgs()
+
+    for f, x, y in solution[0][-1]:
+        imgs[f][x, y] = blue
+
+        frame = cv2.cvtColor(imgs[f], cv2.COLOR_RGBA2BGR)
+        resized = cv2.resize(frame, videodims,
+                             interpolation=cv2.INTER_NEAREST_EXACT)
+
+        for i in range(6):
+            video.write(resized)
 
     video.release()
