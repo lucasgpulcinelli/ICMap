054a3ce4d0323c66ec32c20fa1f9822059d1c212
Matheus Cirillo <matheuscirillo@usp.br>
2024-04-21 21:04:56 -0300

    fixed maze_solver documentation

--- src/maze_solver.py
+++ src/maze_solver.py
@@ -19,8 +19,8 @@ def solveBFS(
     map = np.array([[[True], [True], [True]]])
 
     # from the top of the maze to the bottom
-    path = solveBFS(map, (0, 0, 0), (0, 2, 0))
-    print(path) # prints [(0, 0, 0), (0, 1, 0), (0, 2, 0)]
+    paths_step, border_diff_step = solveBFS(map, (0, 0, 0), (0, 2, 0))
+    print(paths_step[-1]) # prints [(0, 0, 0), (0, 1, 0), (0, 2, 0)]
     '''
 
     path_step, border_step = bfs.bfs(tensor, source, destination)
@@ -37,18 +37,17 @@ def solveAStarEuclidean(
 ) -> Tuple[List[List[Tuple[int, int, int]]], List[List[Tuple[int, int, int]]]]:
     '''
     solveAStar finds a path from source to destination in a boolean walk tensor 
-    using A*. The return is a list of tuples containing the path.
+    using A*. The return is a list of list of tuples of the path at each iteration and
+    a list of lists of tuples of the nodes that can be explored at the next step.
 
     usage example: 
 
     map = np.array([[[True], [True], [True]]])
 
     # from the top of the maze to the bottom
-    path = solveAStar(map, (0, 0, 0), (0, 2, 0))
-    print(path) # prints [(0, 0, 0), (0, 1, 0), (0, 2, 0)]
-
+    paths_step, border_diff_step = solveAStar(map, (0, 0, 0), (0, 2, 0))
+    print(paths_step[-1]) # prints [(0, 0, 0), (0, 1, 0), (0, 2, 0)]
     '''
-    #maze = np.logical_not(tensor)
     maze = tensor.astype(int)
 
     path_step, border_step = a_star.astar(maze, source, destination, 2)
@@ -62,6 +61,20 @@ def solveAStarPartitioned(
     source: Tuple[int, int, int],
     destination: Tuple[int, int, int]
 ) -> Tuple[List[List[Tuple[int, int, int]]], List[List[Tuple[int, int, int]]]]:
+    '''
+    solveAStar finds a path from source to destination in a boolean walk tensor 
+    using a type of partitioned A*. The return is a list of list of tuples of the 
+    path at each iteration and a list of lists of tuples of the nodes that can 
+    be explored at the next step.
+
+    usage example: 
+
+    map = np.array([[[True], [True], [True]]])
+
+    # from the top of the maze to the bottom
+    paths_step, border_diff_step = solveAStarPartitioned(map, (0, 0, 0), (0, 2, 0))
+    print(paths_step[-1]) # prints [(0, 0, 0), (0, 1, 0), (0, 2, 0)]
+    '''
     maze = tensor.astype(int)
     path_step, border_step = a_star.astar_partitioned(
         maze, source, destination, True)
